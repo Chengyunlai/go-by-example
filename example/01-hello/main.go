0196809fc7c16a23890b84b1ba7cacdb6efc0e5c
一卒 <1257997441@qq.com>
2022-10-03 16:13:49 +0000

    变量声明初始化

--- example/01-hello/main.go
+++ example/01-hello/main.go
@@ -1,9 +1,12 @@
+// 创建包main，声明了一个main包
 package main
 
+// 导入内置包文件
 import (
 	"fmt"
 )
 
+// main函数程序入口
 func main() {
 	fmt.Println("hello world")
 }
--- example/02-var/main.go
+++ example/02-var/main.go
@@ -6,16 +6,17 @@ import (
 )
 
 func main() {
+	// 语法：var 变量名 类型(可以省略) = 值
+	var a = "initial" // 自动推导
 
-	var a = "initial"
+	var b, c int = 1, 2 //声明类型
 
-	var b, c int = 1, 2
+	var d = true // 自动推导
 
-	var d = true
+	var e float64 // 声明未赋值
 
-	var e float64
-
-	f := float32(e)
+	// 语法：变量名 := 值
+	f := float32(e) // 简短定义
 
 	g := a + "foo"
 	fmt.Println(a, b, c, d, e, f) // initial 1 2 true 0 0

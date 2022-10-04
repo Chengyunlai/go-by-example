f486897012cd70279a05641c3676112b374a4f1a
chengyunlai <1257997441@qq.com>
2022-10-04 11:10:33 +0800

    变量交换和常量定义

--- example/02-var/main.go
+++ example/02-var/main.go
@@ -22,8 +22,42 @@ func main() {
 	fmt.Println(a, b, c, d, e, f) // initial 1 2 true 0 0
 	fmt.Println(g)                // initialapple
 
+	// 常量的声明
 	const s string = "constant"
 	const h = 500000000
 	const i = 3e20 / h
 	fmt.Println(s, h, i, math.Sin(h), math.Sin(i))
+
+	print()
+	change()
+}
+
+func print(){
+	//var方式声明多变量
+	var a,b,c int
+	a=1 
+	b=2 
+	c=3
+ //也可以写在一行
+ 	// var a1,a2,a3 int =10,20,30
+ //也可以省略类型 根据数据进行类型推导
+	// var a1,a2,a3,a4 =10,20,"ago",30
+ //如果是多种类型 也可以使用集合
+ 	var(
+		 a1 =""
+		 a2 =10
+ )
+ fmt.Println(a,b,c)
+//  fmt.Println(a1,a2,a3)
+//  fmt.Println(a1,a2,a3,a4)
+ fmt.Println(a1,a2)
+}
+
+/**
+ * 交换元素
+ */
+func change(){
+	var a,b = 10,20
+	a,b = b,a
+	fmt.Println(a,b)
 }
